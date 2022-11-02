5604f317f98dd369db30a1a390b900c52a1aec2f
RainerEvan <rainerevan@gmail.com>
2022-11-02 12:36:37 +0700

    memperbaiki

--- frontend/src/app/helpers/auth-guard.ts
+++ frontend/src/app/helpers/auth-guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 
 @Injectable({
@@ -9,7 +9,7 @@ export class AuthGuard implements CanActivate {
 
   constructor(private router: Router, private authService: AuthService) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivate(route: ActivatedRouteSnapshot): boolean {
     const account = this.authService.accountValue;
 
     if(account){
