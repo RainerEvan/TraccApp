62a1e0caa4fb93d09f74783737d7c633d2bf63f1
RAINER EVAN <U545587@bca.co.id>
2023-01-27 16:20:24 +0700

    update config

--- backend/src/main/java/com/project/tracc/config/CorsConfig.java
+++ backend/src/main/java/com/project/tracc/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://10.43.6.167::3096"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://10.43.6.167:3096"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
