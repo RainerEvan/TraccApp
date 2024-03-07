5c148cf3f0939ba44598aa2ded61fb908e425064
RainerEvan <58715474+RainerEvan@users.noreply.github.com>
2024-03-07 17:21:04 +0700

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -7,13 +7,13 @@ export const environment = {
   // apiUrl:'http://localhost:8080/api',
   apiUrl:'http://10.43.6.167:3095/api',
   firebase: {
-    apiKey: "AIzaSyC1AKF1YxGB46Fn_ibpxRA8R-z_qTkh2Fk",
-    authDomain: "tracc-app.firebaseapp.com",
-    projectId: "tracc-app",
-    storageBucket: "tracc-app.appspot.com",
-    messagingSenderId: "552648146786",
-    appId: "1:552648146786:web:7f2d4bc722f5a71ba8b63b",
-    measurementId: "G-84627433P9"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
